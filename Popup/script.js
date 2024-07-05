3aef00d0ec3ab8a9798576ee4237db6fd6b1fe9b
dharshini kuppusamy <113281413+dharshini2309@users.noreply.github.com>
2024-07-05 01:31:53 -0700

    Add files via upload

---
+++ Popup/script.js
@@ -0,0 +1,9 @@
+let popup=document.getElementById("popup");
+
+function Openpopup(){
+    popup.classList.add("open-Popup");
+}
+
+function ClosePopup(){
+    popup.classList.remove("open-Popup");
+}
